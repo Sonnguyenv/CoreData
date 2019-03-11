b79bfdbdd2228181ffc60e4fff00b4f50749e05b
SonNV-D1 <sonnguyenv.mta@gmail.com>
2019-03-11 10:48:07 +0700

    CoreData

---
+++ CoreData/CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreData
+//
+//  Created by SonNV-D1 on 3/11/19.
+//  Copyright © 2019 SonNV-D1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
